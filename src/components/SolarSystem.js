4e9fbed00e68cb8f3ba5c970de793991e5c13e55
Bernardo Marques <bernardomp.dev@gmail.com>
2022-10-21 20:10:41 -0300

    2nd requirement completed.

---
+++ src/components/SolarSystem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SolarSystem extends Component {
+  render() {
+    return (
+      <div data-testid="solar-system">
+        <p>Div Solar System</p>
+      </div>
+    );
+  }
+}
+
+export default SolarSystem;
